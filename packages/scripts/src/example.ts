745608170615cb98ea1b3b82eb2578d39375914f
Ajani Green <ajani.green@outlook.com>
2025-03-06 20:29:48 -0500

    Update README

--- packages/scripts/src/example.ts
+++ packages/scripts/src/example.ts
@@ -1,4 +1,4 @@
 import { Resource } from "sst";
-import { Example } from "@monorepo-template/core/example";
+import { Example } from "@lumi/core/example";
 
 console.log(`${Example.hello()} Linked to ${Resource.MyBucket.name}.`);
